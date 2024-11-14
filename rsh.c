843d690499efa994890d7242095d24a75648dbef
Sean Gilbert <seanlgilbert@icloud.com>
2024-11-13 21:19:25 -0700

    i added my code

--- rsh.c
+++ rsh.c
@@ -15,7 +15,11 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	for (int i = 0; i < N; i++) {
+        if (strcmp(cmd, allowed[i]) == 0) {
+            return 1;
+        }
+    }
 	return 0;
 }
 
@@ -25,6 +29,7 @@ int main() {
     // Add variables as needed
 
     char line[256];
+    char *argv[21];
 
     while (1) {
 
@@ -36,10 +41,63 @@ int main() {
 
 	line[strlen(line)-1]='\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+	int argc = 0;
+        char *token = strtok(line, " ");
+        while (token != NULL && argc < 20) {
+            argv[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;  // Null-terminate argument list
+
+        // Check if the command is allowed
+        if (!isAllowed(argv[0])) {
+            printf("NOT ALLOWED!\n");
+            continue;
+        }
+
+        // Handle built-in commands: cd, exit, help
+        if (strcmp(argv[0], "cd") == 0) {
+            if (argc > 2) {
+                printf("-rsh: cd: too many arguments\n");
+            } else if (argc == 2) {
+                if (chdir(argv[1]) != 0) {
+                    perror("cd failed");
+                }
+            }
+            continue;
+        } else if (strcmp(argv[0], "exit") == 0) {
+            return 0;  // Exit the shell
+        } else if (strcmp(argv[0], "help") == 0) {
+            printf("The allowed commands are:\n");
+            for (int i = 0; i < N; i++) {
+                printf("%s\n", allowed[i]);
+            }
+            continue;
+        }
+
+        // Spawn processes for the first 9 commands
+        pid_t pid;
+        int status;
+        posix_spawnattr_t attr;
+
+        // Initialize spawn attributes
+        posix_spawnattr_init(&attr);
+
+        if (posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ) != 0) {
+            perror("posix_spawnp failed");
+        } else {
+            // Wait for the spawned process to finish
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("waitpid failed");
+            }
+            if (WIFEXITED(status)) {
+                printf("Process exited with status %d\n", WEXITSTATUS(status));
+            }
+        }
+
+        // Clean up spawn attributes
+        posix_spawnattr_destroy(&attr);
+    
 
     }
     return 0;
